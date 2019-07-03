6d7993e8d5107a4f26b5c10c2bc951b73972a25c
jose luis <cortezluisjose@gmail.com>
2019-07-03 01:19:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,16 +12,14 @@ def pioneer
 end
 
 def id_generator
-  id_generator[:id]
+  id_generator.object_id
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
-end
+ my_hash_creator[:key]
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
-end
+ 
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
